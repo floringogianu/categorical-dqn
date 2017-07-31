5d5fee5ee299e22f48615fb2f9465452fc63adb2
Florin Gogianu <florin.gogianu@gmail.com>
2017-07-31 14:53:45 +0300

    Consistent naming.

--- policy_evaluation/deterministic.py
+++ policy_evaluation/deterministic.py
@@ -2,14 +2,14 @@ from torch.autograd import Variable
 
 
 class DeterministicPolicy(object):
-    def __init__(self, estimator):
-        """Assumes estimator returns an autograd.Variable"""
+    def __init__(self, policy):
+        """Assumes policy returns an autograd.Variable"""
 
         self.name = "DP"
-        self.estimator = estimator
+        self.policy = policy
 
     def get_action(self, state_batch):
         """ Takes best action based on estimated state-action values."""
-        q_val, argmax_a = self.estimator(
+        q_val, argmax_a = self.policy(
                 Variable(state_batch, volatile=True)).data.max(1)
         return (q_val[0, 0], argmax_a[0, 0])
